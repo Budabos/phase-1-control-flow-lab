554f9749ca6109792d2a9ea1aed094a0f0b4cfd4
caroline opiyo <opiyocaroline20@gmail.com>
2023-09-19 06:31:43 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+    let message;
+  
+    if (feet <= 400) {
+      message = 'This one is on me!';
+    } else if (feet > 2000 && feet <= 2500) {
+      message = 'I will gladly take your thirty bucks.';
+    } else if (feet > 2500) {
+      message = 'No can do.';
+    }
+  
+    return message;
+  }
+console.log(scuberGreetingForFeet(200)); 
+console.log(scuberGreetingForFeet(2200)); 
+console.log(scuberGreetingForFeet(2600));
+  
+    
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
+console.log(ternaryCheckCity('NYC')); 
+console.log(ternaryCheckCity('Los Angeles'));
+console.log(ternaryCheckCity('Chicago')); 
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous')); 
+console.log(switchOnCharmFromTip('average')); 
